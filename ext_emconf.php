d960c37edc86f6c408aec468332ff9ceddac3e4b
Roman <rb@buechler.pro>
2018-12-17 19:01:19 +0100

    [TASK] Rise version to 9.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => '0',
     'createDirs'       => '',
     'clearCacheOnLoad' => 0,
-    'version'          => '9.0.1',
+    'version'          => '9.0.2',
     'constraints'      => [
         'depends'   => [
-            'typo3'             => '9.5.0-9.99.99'
+            'typo3'         => '9.5.0-9.99.99'
         ],
         'conflicts' => [
         ],
