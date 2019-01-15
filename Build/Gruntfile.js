543c2bf9edd0a2f7135a483e4defff0f80efc617
Roman <rb@buechler.pro>
2019-01-15 10:37:00 +0100

    [FEATURE] Add grunt generator for Changelog.md

--- Build/Gruntfile.js
+++ Build/Gruntfile.js
@@ -9,7 +9,16 @@ module.exports = function(grunt) {
             root: '../',
             node: 'node_modules/',
             resources: '<%= paths.root %>Resources/',
-            js: '<%= paths.resources %>Public/JavaScript/'
+            icons: '<%= paths.resources %>Public/Icons/',
+            fonts: '<%= paths.resources %>Public/Fonts/',
+            js: '<%= paths.resources %>Public/JavaScript/',
+            doc: '<%= paths.root %>Documentation/'
+        },
+        cssmin: {
+            moosaicicon: {
+                src: '<%= paths.fonts %>moosaicicon.css',
+                dest: '<%= paths.fonts %>moosaicicon.min.css'
+            }
         },
         uglify: {
             options: {
@@ -30,20 +39,69 @@ module.exports = function(grunt) {
                 files: '<%= paths.js %>Src/jquery.general.js',
                 tasks: 'uglify:general'
             },
+        },
+        webfont: {
+            moosaicicon: {
+                src: '<%= paths.icons %>Font/*.svg',
+                dest: '<%= paths.fonts %>',
+                options: {
+                    font: 'moosaicicon',
+                    template: 'templates/font.css',
+                    fontFamilyName: 'MoosaicIcon',
+                    engine: 'node',
+                    autoHint: false,
+                    htmlDemo: false,
+                    templateOptions: {
+                        baseClass: 'ucicon',
+                        classPrefix: 'ucicon-'
+                    }
+                }
+            }
+        },
+        /**
+         * @see https://github.com/ericmatthys/grunt-changelog
+         * @alternative https://github.com/rafinskipg/git-changelog
+         */
+        changelog: {
+            sample: {
+                options: {
+                    fileHeader: '# Changelog',
+                    after: '2018-12-01',
+                    before: '2030-01-01',
+                    dest: '<%= paths.doc %>Changelog.md',
+                    logArguments: [
+                        '--pretty=%ci %s (Commit %h by %an)',
+                        '--no-merges',
+                        '--abbrev-commit'
+                    ],
+                    template: '{{> features}}\n---\n**Hint:** The md-files might be viewed by ' +
+                        'http://md.paratte.ch/github-markdown-parser.php',
+                    featureRegex: /^(.*)$/gim,
+                    partials: {
+                        features: '{{#if features}}{{#each features}}{{> feature}}{{/each}}{{else}}{{> empty}}{{/if}}\n',
+                        feature: '- {{this}} {{this.date}}\n'
+                    }
+                }
+            }
         }
     });
 
     /**
      * Register tasks
      */
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-changelog');
 
     /**
      * Grunt update task
      */
+    grunt.registerTask('css', ['cssmin']);
     grunt.registerTask('js', ['uglify']);
-    grunt.registerTask('build', ['js']);
+    grunt.registerTask('doc', ['changelog']);
+    grunt.registerTask('build', ['webfont','css','js','doc']);
     grunt.registerTask('default', ['build']);
 
 };
